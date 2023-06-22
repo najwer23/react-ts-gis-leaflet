f9c32dd90f1430964c9c405167b57678fdab012a
Mariusz Najwer <najwer23@gmail.com>
2023-06-22 21:23:40 +0200

    working select

--- src/nav/Nav.tsx
+++ src/nav/Nav.tsx
@@ -1,7 +1,8 @@
 import './Nav.css';
 import { useEffect, useState } from "react";
 import { Button } from '../buttons/Button';
-
+import { useWindowSize } from "../utils/Utils";
+import Select from 'react-select';
 
 export const Nav = () : JSX.Element => {
 	return (
@@ -13,8 +14,27 @@ export const Nav = () : JSX.Element => {
 	)
 }
 
+
+
+
+
+
+const OptionListForSelect = [
+	{ value: "1", label: "Pozwolenia na budowę" },
+	{ value: "2", label: "Plany zagospodarowania terenu" },
+];
+
+
+
+
+
+
+
+
+
 const Menu = (): JSX.Element => {
 	const [isMenuOpen, setIsMenuOpen] = useState(false);
+	const [width,] = useWindowSize();
 
 	useEffect(() => {
 		let body = document.querySelector("body")
@@ -27,6 +47,38 @@ const Menu = (): JSX.Element => {
 
 	function toggleMenu() {
 		setIsMenuOpen(!isMenuOpen);
+	}
+
+
+
+
+	const OptionListForSelectCSS = {
+		control: (base: any) => ({
+			...base,
+			border: 0,
+			boxShadow: 'none'
+		}),
+		option: (base: any, state: any) => ({
+			...base,
+			width: "200px",
+			minWidth: "100%",
+			backgroundColor: state.isSelected ? "#c4c4c4" : "",
+			'&:hover': {
+				backgroundColor: state.isSelected ? '#c4c4c4' : 'whitesmoke',
+				cursor: "pointer"
+			},
+			color: state.isSelected ? "black" : "",
+		}),
+		input: (base: any, state: any) => ({
+			...base,
+			width: "max-content",
+			minWidth: "100%",
+		}),
+		menu: (base: any) => ({
+			...base,
+			width: "max-content",
+			minWidth: "100%",
+		}),
 	}
 
 	return (
@@ -55,7 +107,24 @@ const Menu = (): JSX.Element => {
 
 					<div className='layout1'>
 						<ul className='popup-menu-list'>
-							<li>halo</li>
+							<li>
+								<h2 className='fluid-h2'>Options</h2>
+
+								<div style={{
+									width: width <= 575.98 ? "100%" : "400px",
+									marginBottom: "30px"
+								}}>
+									<Select
+										styles={OptionListForSelectCSS}
+										defaultValue={OptionListForSelect[0]}
+										// onChange={setCoords}
+										options={OptionListForSelect}
+										isSearchable={false}
+									/>
+								</div>
+
+
+							</li>
 						</ul>
 					</div>
 				</div>
